b01c9b442e1a450286c0d4778c653cc60610d003
alexandre DT <alexandredt79@gmail.com>
2016-02-29 17:30:56 -0500

    trying to fix participation, still not wirking, commiting because it's 5:30

--- app/controllers/spree/participations_controller.rb
+++ app/controllers/spree/participations_controller.rb
@@ -8,9 +8,26 @@ module Spree
     end
 
     def create
-      @participation = Spree::Participation.find(params[:id])
-      @participation.subscribed = true
+      unless params[:terms]
+        flash[:notice] = "vous n'avez pas accepté les termes"
+        redirect_to(:back)
+      end
+      @participation = Spree::Participation.new(participation_params)
+
+      unless @participation.save
+        flash[:notice] = "le formulaire a été mal rempli"
+        redirect_to(:back)
+      end
+
     end
+
+
+
+
+    def participation_params
+      params.require(:participation).permit(:name, :email, :zip)
+    end
+
 
   end
 end
--- app/models/spree/participation.rb
+++ app/models/spree/participation.rb
@@ -1,13 +1,8 @@
 module Spree
   class Participation < Spree::Base
-    extend FriendlyId
-    friendly_id :slug_candidates, use: [:slugged, :finders]
     belongs_to :contest
     validates_uniqueness_of :email, :scope => :contest_id
 
-    def slug_candidates
-      [:email]
-    end
   end
 end
 
