7e259cf6c9afdb9eaf180fe49880fc4729a6e439
Billybones79 <alexandredt79@gmail.com>
2016-03-02 12:29:19 -0500

    modification de delete de parti

--- app/controllers/spree/admin/participation_controller.rb
+++ app/controllers/spree/admin/participation_controller.rb
@@ -7,6 +7,13 @@ module Spree
         @participations = @contest.participations
       end
 
+      def destroy
+        @participations = Spree::Parcticipation.find(params[:id])
+        @contest = @participations.contest
+        @participations.dstory
+        redirect_to admin_contest_path(@contest)
+      end
+
     end
   end
 end
